0d2566711e9ede5f7dc06dd2c69a816273e6ac38
Kristian Lein-Mathisen <kristianlein@lmdig>
2012-06-04 20:42:57 -0700

    bind-utils: added conversion from lists->f32vectors and back
    application-level should be float/double-precision agnostic ... is
    this feasable?

--- fann.scm
+++ fann.scm
@@ -1,14 +1,41 @@
 
 (module fann *
- (import chicken scheme foreign srfi-4 bind)
+ (import chicken scheme foreign)
+ (import bind)
+ (use srfi-4 lolevel)
+
 #>
 #include <fann.h>
 <#
 
-(bind-options default-renaming: "fann:")
+(bind-options default-renaming: "fann:" export-constants: #t)
 (bind-rename/pattern "^fann-" "")
 (bind-include-path "./include")
 (bind-file "include/fann.h")
+
+(define fann:sizeof-uint (foreign-value "sizeof(unsigned int)" int))
+(define fann:sizeof-fann-type (foreign-value "sizeof(fann_type)" int))
+
+(define (fann:create-standard . layer-sizes)
+ (fann:create-standard-array (length layer-sizes)
+                             (location
+                              (u32vector->blob
+                               (list->u32vector layer-sizes)))))
+
+(define (pointer->blob pointer bytes)
+  (let ([b (make-blob bytes)])
+    (move-memory! pointer b bytes)
+    b))
+
+(define fann:run
+  (let ([fann:run* fann:run])
+    (lambda (ann inputs)
+      (f32vector->list
+       (blob->f32vector/shared
+        (pointer->blob (fann:run* ann (list->f32vector inputs))
+                       (fx* (foreign-value "sizeof(fann_type)" int)
+                            (fann:get-num-output ann))))))))
+
 
 
  )
